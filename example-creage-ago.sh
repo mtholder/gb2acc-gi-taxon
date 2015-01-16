f33c378b7f552ad9e124d34cc68f54d10a786701
Mark T. Holder <mtholder@gmail.com>
2015-01-16 15:35:44 +0100

    example script cleans up after itself

--- example-creage-ago.sh
+++ example-creage-ago.sh
@@ -3,18 +3,28 @@
 set -x
 ff='failed-ago-err-list.txt'
 rm -f "${ff}"
+rm -f "COMBINED-ago.tsv"
+rm -f "COMBINED-ago-err.txt"
 for f in *.seq.xz
 do
     fn=$(echo $f | cut -d. -f1-2)
     echo $fn
+    uncompressed_file=0
     if ! test -f "$fn"
     then
         xz --decompress --stdout "$f" > "$fn" || exit
+        uncompressed_file=1
     fi
     if ! gb2acc-gi-taxon "$fn" > "${fn}-ago.tsv" 2>"${fn}-ago-err.txt"
     then
         echo "$fn" >> "$ff"
     fi
+    if test $uncompressed_file -eq 1
+    then
+        rm "$fn"
+    fi
+    cat "${fn}-ago.tsv" >> "COMBINED-ago.tsv"
+    cat "${fn}-ago-err.txt" >> "COMBINED-ago-err.txt"
 done
 if test -f "${ff}"
 then
